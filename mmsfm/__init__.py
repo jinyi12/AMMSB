7b18fbdda609af2b4b35ff73f0dad64f743dccf6
Jin Yi Yong <jinyi12@outlook.com>
2026-01-23 18:04:31 -0500

    archieve deep kernel

--- mmsfm/__init__.py
+++ mmsfm/__init__.py
@@ -2,8 +2,7 @@ __version__ = '0.0.1'
 
 from .diffusion_map_sampler import DiffusionMapTrajectorySampler
 from .psi_provider import PsiProvider
-from .deep_kernel import AttentionWeightNet, DeepKernelEncoder
-from .preimage_decoder import LowRankSPDPreconditioner, PreimageEnergyDecoder
+from .preimage_decoder import LowRankSPDPreconditioner, PreimageEnergyDecoder, TimeConditionedDecoder
 from .precomputed_cfm import PrecomputedConditionalFlowMatcher
 from .flow_objectives import (
     VelocityPredictionObjective,
@@ -16,10 +15,11 @@ __all__ = [
     '__version__',
     'DiffusionMapTrajectorySampler',
     'PsiProvider',
-    'AttentionWeightNet',
-    'DeepKernelEncoder',
+    'DiffusionMapTrajectorySampler',
+    'PsiProvider',
     'LowRankSPDPreconditioner',
     'PreimageEnergyDecoder',
+    'TimeConditionedDecoder',
     'PrecomputedConditionalFlowMatcher',
     'VelocityPredictionObjective',
     'XPredictionVelocityMSEObjective',
